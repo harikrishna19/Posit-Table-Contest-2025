75ed1f61f573a26c77f4aaa44a2edcd64dfaf755
harikrishna19 <krishna19599@gmail.com>
2025-09-30 23:10:37 +0530

    Update sub-func

--- R/func_subtables.R
+++ R/func_subtables.R
@@ -37,3 +37,30 @@ merge_tables<-function(inc_data,out_data,teams,pl_inc_data){
   combined$team_logo<-team_logos
   return(combined)
 }
+
+
+
+
+
+update_cols <- function(data) {
+  data <- data %>% dplyr::mutate(
+    Age = case_when(
+      Age <= 21 ~ "U21",
+      Age <= 25 ~ "22-25",
+      Age <= 30 ~ "26-30",
+      TRUE ~ "30+"
+    ),
+    # Add emojis for position
+    Position = case_when(
+      Position == "Goalkeeper" ~ "🧤 GK",
+      Position %in% c("Centre-Back","Left-Back","Right-Back") ~ "🛡️ DEF",
+      Position %in% c("Left Midfield","Right Midfield","Defensive Midfield","Attacking Midfield") ~ "🎯 MID",
+      Position %in% c("Centre-Forward","Second Striker") ~ "⚽ FWD",
+      Position %in% c("Right Winger","Left Winger") ~ "⚽ FWingers",
+      TRUE ~ Position  # fallback for any other positions
+    )
+  )
+  return(data)
+}
+
+update_cols(pl_incomings)
